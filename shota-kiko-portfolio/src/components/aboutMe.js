aee18fbf2b20a03a726b45e340193fd424407a9d
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-04-03 18:59:44 -0500

    added logos for tech stack/minimal styling

--- shota-kiko-portfolio/src/components/aboutMe.js
+++ shota-kiko-portfolio/src/components/aboutMe.js
@@ -30,7 +30,37 @@ const AboutMe = () => (
             </p>
         </div>
         
-        <div className="aboutRight">right</div>
+        <div className="aboutRight">
+            <div className="techHeader">
+                <h4>Technologies</h4>
+            </div>
+            
+            <div className="iconContainer">
+                <div className="firstRow">
+                    <i className="devicon-html5-plain-wordmark colored"></i>
+                    <i class="devicon-css3-plain-wordmark colored"></i>
+                    <i class="devicon-javascript-plain colored"></i>
+                    <i class="devicon-react-original-wordmark colored"></i>
+                    <i class="devicon-nodejs-plain-wordmark colored"></i>
+                </div>
+                
+                <div className="secondRow">
+                    <i class="devicon-express-original-wordmark colored"></i>
+                    <i class="devicon-python-plain-wordmark colored"></i>
+                    <i class="devicon-django-plain-wordmark colored"></i>
+                    <i class="devicon-postgresql-plain-wordmark colored"></i>
+                    <i class="devicon-heroku-plain-wordmark colored"></i>
+                </div>
+                
+                <div className="thirdRow">
+                    <i class="devicon-git-plain-wordmark colored"></i>
+                    <i class="devicon-github-plain-wordmark colored"></i>
+                    <i class="devicon-less-plain-wordmark colored"></i>
+                    <i class="devicon-sass-original colored"></i>
+                    <i class="devicon-bootstrap-plain-wordmark colored"></i>
+                </div>
+            </div>
+        </div>
         
         <div className="aboutLink">
             <Link to="/">Back to top</Link>

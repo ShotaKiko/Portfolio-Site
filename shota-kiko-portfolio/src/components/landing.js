62fff4c3130a2fbebaaacb2d2be97c08c172e70a
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-06-08 19:59:25 -0500

    increased landing text size on larger displays

--- shota-kiko-portfolio/src/components/landing.js
+++ shota-kiko-portfolio/src/components/landing.js
@@ -7,22 +7,20 @@ const Landing = () => (
     <section className="landing" id="home">
         <div className="overlay"></div>
         <div className="landing-text-wrapper">
-            <div>
-                <h2 className="landing-text">
-                    Hello, I'm Shota Kikozashvili.
-                    <br></br>
-                    I'm a Full-Stack Web Developer.
-                    <br></br>
-                </h2>
-            </div>
-            <div>
-                <div className="landingButton">
-                    <Link className="fadeIn" activeClass="active" to="about" spy={true} smooth={true} duration={1000}>
-                        <Button color="info">
-                            Learn More <ArrowForwardIcon className="rotate" />
-                        </Button>
-                    </Link>
-                </div>
+            
+            <h2 className="landing-text">
+                Hello, I'm Shota Kikozashvili.
+                <br></br>
+                I'm a Full-Stack Web Developer.
+                <br></br>
+            </h2>
+        
+            <div className="landingButton">
+                <Link className="fadeIn" activeClass="active" to="about" spy={true} smooth={true} duration={1000}>
+                    <Button color="info">
+                        Learn More <ArrowForwardIcon className="rotate" />
+                    </Button>
+                </Link>
             </div>
         </div>
     </section>
